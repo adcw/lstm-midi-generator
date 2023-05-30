66774c1e4dd78e4ba2cb3383d457e9ab101ca496
Adrian Ćwiąkała <61947288+adcw@users.noreply.github.com>
2023-05-30 18:10:48 +0200

    Test reading MIDI data

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
-import src.midi_func as mf
+from src.dataset import read_datasets
 from src.utils import get_midi_filenames
-from src.dataset import create_dataset, get_tensor_dataset
-from src.model import MusicModel
-import pretty_midi
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
+from src.midi_func import notes_to_midi
 
 
 if __name__ == '__main__':
     vocab_size = 128
     seq_len = 40
 
-    filenames = get_midi_filenames('samples', ['Swing Beats'])
-    all_notes = create_dataset(filepaths=filenames)
-    train_ds = get_tensor_dataset(all_notes, vocab_size=vocab_size, seq_len=seq_len)
+    filenames = get_midi_filenames(main_dir='samples', subdirs=['Swing Beats'])
 
-    music_model = MusicModel(seq_len=seq_len)
-    music_model.fit(train_ds)
+    all_notes = read_datasets(filepaths=filenames[:1])[0]
 
-    n = 10
-    for i in range(n):
-        generated_notes = music_model.generate_notes(all_notes[:seq_len])
-        mf.notes_to_midi(generated_notes, 'Acoustic Grand Piano', out_file=f'output/generated{i}.mid')
+    # notes_to_midi(all_notes, "Acoustic Grand Piano", "./output/test.mid")
     pass
