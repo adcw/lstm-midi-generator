0b75c41a1b4f553a3decfb4f8aa49f4935f27340
Przemek <rysiulaw@gmail.com>
2023-05-30 20:56:01 +0200

    dataset prototype

--- src/dataset.py
+++ src/dataset.py
@@ -1,38 +1,32 @@
 from src.midi_func import midi_to_notes
-from pandas import DataFrame
-
+import pandas as pd
+import numpy as np
 
 def read_datasets(filepaths: list[str]):
     return [midi_to_notes(f) for f in filepaths]
 
 
-def process_dataset(df: DataFrame) -> DataFrame:
+def process_dataset(df: pd.DataFrame) -> pd.DataFrame:
     """
-
+    notes['pitch'].append(note.pitch)
+    notes['start'].append(start)
+    notes['start offset'].append(start % 1)
+    notes['end'].append(end)
+    notes['end offset'].append(start % 1)
+    notes['velocity'].append(note.velocity)
     Input data format
-            pitch      start         start offset   end         end offset  velocity
+            pitch      start         start offset   end         end offset       velocity
     0       36         0.000000      0.000000       0.527083    0.000000         127
     1       42         0.012500      0.012500       0.252083    0.012500         127
     2       42         0.679167      0.679167       0.918750    0.679167          91
     3       36         0.750000      0.750000       1.277083    0.750000         127
     4       38         1.000000      0.000000       1.527083    0.000000         127
-    ..     ...         ...           ...            ...         ...              ...
-    103     36         30.250000     0.250000       30.277083   0.250000         127
-    104     42         30.679167     0.679167       30.918750   0.679167          91
-    105     38         31.000000     0.000000       31.527083   0.000000         127
-    106     42         31.012500     0.012500       31.252083   0.012500         127
-    107     42         31.679167     0.679167       31.918750   0.679167          91
 
     Output data format:
-
     t    offset 36       38    42
     0.0  0.1    [2, 127] [0, 0] [0, 0]
-    0.2  0.2    [1, 127] [0, 0] [0, 0]
+    0.3  0.2    [1, 127] [0, 0] [0, 0]
     0.4  0.3    [1, 127] [0, 0] [0, 0]
-    0.6  0.4    [0, 0]   [0, 0] [0, 0]
-    0.7  0.5    [0, 0]   [0, 0] [0, 0]
-    ...
-    1.5  1.3    [0, 0]   [0, 0] [0, 0]
 
     t - time in beats
     offset - beat offset (how much the event is shifted from the beat time)
@@ -56,5 +50,42 @@ def process_dataset(df: DataFrame) -> DataFrame:
     :param df: the dataframe used to generate training data
     :return: generated training data
     """
+    col = ['time', 'offset']
+    col.extend(df['pitch'].unique())
+    uniq_pitch = df['pitch'].unique()
+    output = pd.DataFrame(columns=col)
+    output['time'] = pd.concat([df['start'], df['end']], axis=0)
+    output['offset'] = pd.concat([df['start offset'], df['end offset']], axis=0)
+    output = output.sort_values(by=['time'])
+    output = output.reset_index(drop=True)
+    output.iloc[2:] = output.iloc[2:].astype(object)
 
-    # for note in
+    # output = output.reindex(output['time'])
+    a = df['start']
+    for i,row in df.iterrows():
+        pitch = row['pitch']
+        velocity = row['velocity']
+        start = row['start']
+        end = row['end']
+        index = np.flatnonzero(output['time'] == start)
+        output.at[index[0], pitch] = [2,velocity]
+        index = np.flatnonzero(output['time'] == end)
+        output.at[index[0], pitch] = [0, 0]
+
+    for i in range(0,output.shape[0]-1):
+        # row:
+        row = output.iloc[i,:]
+        next_row = output.iloc[i+1,:]
+
+    pass
+
+    # for i,row in df.iterrows():
+    #     pitch = row['pitch']
+    #     velocity = row['velocity']
+    #     start = row['start']
+    #     end = row['end']
+    #     loc = output.loc[output['time'] == start]
+    #     loc[pitch] = [2,velocity]
+    #     loc = output.loc[output['time'] == end]
+    #     loc[pitch] = [0, 0]
+    #     pass
