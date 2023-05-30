5067614b462d75098c6c261b02b5d32ed0c2888f
Adrian Ćwiąkała <61947288+adcw@users.noreply.github.com>
2023-05-30 13:27:50 +0200

    rename seq_length to seq_len

--- src/dataset.py
+++ src/dataset.py
@@ -8,12 +8,12 @@ def create_dataset(filepaths: list[str]):
     return pd.concat([midi_to_notes(f) for f in filepaths])
 
 
-def get_tensor_dataset(dataset: pd.DataFrame, key_order: list[str] | None = None, seq_length: int = 25, vocab_size=128):
+def get_tensor_dataset(dataset: pd.DataFrame, key_order: list[str] | None = None, seq_len: int = 25, vocab_size=128):
     if key_order is None:
         key_order = ['pitch', 'step', 'duration']
 
     notes_ds = _get_dataset(dataset, key_order)
-    seq_ds = _create_sequences(notes_ds, seq_length, key_order=key_order, vocab_size=vocab_size)
+    seq_ds = _create_sequences(notes_ds, seq_len, key_order=key_order, vocab_size=vocab_size)
 
     return seq_ds
 
@@ -26,19 +26,19 @@ def _get_dataset(dataset: pd.DataFrame, keys: list[str] | None = None):
     return notes_ds
 
 
-def _create_sequences(dataset: tf.data.Dataset, seq_length: int, key_order: list[str],
+def _create_sequences(dataset: tf.data.Dataset, seq_len: int, key_order: list[str],
                       vocab_size=128) -> tf.data.Dataset:
     """Returns TF Dataset of sequence and label examples."""
-    seq_length = seq_length + 1
+    seq_len = seq_len + 1
 
     n_notes = len(dataset)
 
     # Take 1 extra for the labels
-    windows = dataset.window(seq_length, shift=1, stride=1,
+    windows = dataset.window(seq_len, shift=1, stride=1,
                              drop_remainder=True)
 
     # `flat_map` flattens the" dataset of datasets" into a dataset of tensors
-    flatten = lambda x: x.batch(seq_length, drop_remainder=True)
+    flatten = lambda x: x.batch(seq_len, drop_remainder=True)
     sequences = windows.flat_map(flatten)
 
     # Normalize note pitch
@@ -57,7 +57,7 @@ def _create_sequences(dataset: tf.data.Dataset, seq_length: int, key_order: list
     seq_ds = sequences.map(split_labels, num_parallel_calls=tf.data.AUTOTUNE)
 
     batch_size = 64
-    buffer_size = n_notes - seq_length  # the number of items in the dataset
+    buffer_size = n_notes - seq_len  # the number of items in the dataset
     train_ds = (seq_ds
                 .shuffle(buffer_size)
                 .batch(batch_size, drop_remainder=True)
