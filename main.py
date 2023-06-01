8bf5b745a0e5dc861a9634e23a9db686be2df57d
Adrian Ćwiąkała <61947288+adcw@users.noreply.github.com>
2023-06-01 22:03:22 +0200

    Global params

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ from src.dataset import read_datasets, process_dataset, r_process_dataset
 from src.utils import get_midi_filenames
 from src.midi_func import notes_to_midi
 
+VOCAB_SIZE = 128
+SEQ_LEN = 40
+RESOLUTION = 480
+TEMPO = 172
+
 
 if __name__ == '__main__':
-    vocab_size = 128
-    seq_len = 40
-
     filenames = get_midi_filenames(main_dir='samples', subdirs=['Swing Beats'])
 
     all_notes = read_datasets(filepaths=filenames[:1])[0]
     a = process_dataset(all_notes)
     b = r_process_dataset(a)
-    # notes_to_midi(all_notes, "Acoustic Grand Piano", "./output/test.mid")
+    notes_to_midi(b, "Acoustic Grand Piano", "./output/test.mid", tempo=TEMPO, resolution=RESOLUTION)
     pass
