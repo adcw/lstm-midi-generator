253ba73705be4f619f7843572e8d67419eed6dde
Adrian Ćwiąkała <61947288+adcw@users.noreply.github.com>
2023-06-05 23:12:50 +0200

    providing scaler to training_sequence function

--- src/data_sequences.py
+++ src/data_sequences.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
+from src.utils import ColumnScaler
 
 
 def window(df: pd.DataFrame, size: int = 10, stride: int = 1) -> np.array:
@@ -13,19 +14,23 @@ def window(df: pd.DataFrame, size: int = 10, stride: int = 1) -> np.array:
     return np.array(result).astype(np.float)
 
 
-def training_sequence(df: pd.DataFrame, input_size: int = 20, output_size: int = 1, window_stride: int = 1):
-    scaler = MinMaxScaler()
+def training_sequence(df: pd.DataFrame, input_len: int = 20, output_len: int = 1, window_stride: int = 1,
+                      scaler: ColumnScaler | None = None):
     vel_columns_count = int((df.shape[1] - 2) / 2)
-    colnames_to_scale = df.columns[:1].to_list() + df.columns[-vel_columns_count:].to_list()
-    scaled = scaler.fit_transform(df[colnames_to_scale])
+    colnames_to_scale = df.columns[-vel_columns_count:].to_list()
 
-    df[colnames_to_scale] = scaled
+    if scaler is None:
+        scaler = ColumnScaler()
+        scaler.fit(df, cols=colnames_to_scale)
 
-    windowed = window(df, size=input_size + output_size, stride=window_stride)
+    df = scaler.transform(df)
 
-    xs = windowed[:, :input_size, :]
-    ys = windowed[:, input_size:, :]
+    windowed = window(df, size=input_len + output_len, stride=window_stride)
 
-    return xs, ys
+    xs = windowed[:, :input_len, :]
+    ys = windowed[:, input_len:, :]
+    ys = np.delete(ys, 1, axis=2)
+
+    return xs, ys, scaler
 
     pass
