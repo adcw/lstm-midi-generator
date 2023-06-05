0e32d68df32331d498191617b0af1dce50f759f1
Adrian Ćwiąkała <61947288+adcw@users.noreply.github.com>
2023-06-05 23:24:37 +0200

    Add colum scaler

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,48 @@
 import pathlib
 import glob
 import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
 
 def get_midi_filenames(main_dir: str, subdirs: list[str] | None = None):
     data_dir = pathlib.Path(main_dir)
     if subdirs is None:
-        return glob.glob(str(data_dir/'*/*.mid*'))
+        return glob.glob(str(data_dir / '*/*.mid*'))
 
     return np.array([glob.glob(str(main_dir + '/' + d + '/*.mid')) for d in subdirs]).flatten()
+
+
+class ColumnScaler(MinMaxScaler):
+    def __init__(self):
+        super().__init__()
+        self.cols: list[str] | None = None
+        self.indexes: list[int] | None = None
+
+    def fit(self, df: pd.DataFrame, y=None, cols: list[str] | None = None):
+        self.cols = cols
+        self.indexes = [df.columns.get_loc(col) for col in self.cols] if cols is not None else None
+
+        df = self._pick_cols(df) if cols is not None else df
+        return super().fit(df, y)
+
+    def transform(self, df: pd.DataFrame):
+        to_transform = self._pick_cols(df) if self.cols is not None else df
+        transformed = super().transform(to_transform)
+        df.iloc[:, self.indexes] = transformed
+        return df
+
+    def inverse_transform(self, row: np.ndarray):
+        row_to_transform = self._pick_cols(row)
+        row_to_transform = row_to_transform.reshape(1, -1)
+        transformed = super().inverse_transform(row_to_transform)
+
+        row[self.indexes] = transformed
+        return row
+
+    def _pick_cols(self, data: pd.DataFrame | np.ndarray | None = None):
+        if isinstance(data, pd.DataFrame):
+            data = data[self.cols]
+        if isinstance(data, np.ndarray):
+            data = data[self.indexes]
+        return data
