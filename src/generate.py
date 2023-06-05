1495e1d5d881e8d27c12ed995a352693f37ccee5
Adrian Ćwiąkała <61947288+adcw@users.noreply.github.com>
2023-06-05 23:18:09 +0200

    Generation (WIP)

--- src/generate.py
+++ src/generate.py
@@ -2,12 +2,13 @@ import numpy as np
 import pandas as pd
 from keras.models import Model
 from sklearn.preprocessing import MinMaxScaler
+from src.utils import ColumnScaler
 
 from src.data_sequences import training_sequence
 from src.dataset import notes_to_dataset
 
 
-def generate(model: Model, scaler: MinMaxScaler, sample_notes: pd.DataFrame, input_len: int,
+def generate(model: Model, scaler: ColumnScaler, sample_notes: pd.DataFrame, input_len: int,
              output_len: int):
     dataset = notes_to_dataset(sample_notes)
     xs, _, _ = training_sequence(dataset, scaler=scaler, input_len=input_len, output_len=output_len)
@@ -17,8 +18,8 @@ def generate(model: Model, scaler: MinMaxScaler, sample_notes: pd.DataFrame, inp
     xs_reshaped = np.reshape(xs, (-1, xs.shape[0], xs.shape[1]))
 
     pred = model.predict(xs_reshaped)
-    np.insert(pred, 1, pred[0] % 1)
+    pred = np.insert(pred, 1, pred[:, 0] % 1)
 
-    rescaled = scaler.inverse_transform(pred)
+    # rescaled = scaler.inverse_transform(pred)
 
     pass
