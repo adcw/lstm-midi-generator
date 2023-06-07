c2166c5836a8687d95f6eef5b77dc6211b37794d
Adrian Ćwiąkała <61947288+adcw@users.noreply.github.com>
2023-06-07 22:44:28 +0200

    Add generate_notes function, fix predict note function

--- src/generate.py
+++ src/generate.py
@@ -7,19 +7,37 @@ from src.utils import ColumnScaler
 from src.data_sequences import training_sequence
 from src.dataset import notes_to_dataset
 
+from tqdm import tqdm
 
-def predict_note(model: Model, scaler: ColumnScaler, sample_notes: pd.DataFrame, input_len: int,
-                 output_len: int):
-    dataset = notes_to_dataset(sample_notes)
-    xs, _, _ = training_sequence(dataset, scaler=scaler, input_len=input_len, output_len=output_len)
 
-    xs = xs[0]
+def predict_note(model: Model, scaler: ColumnScaler, sample_notes: np.ndarray):
+    xs = sample_notes
+    xs_reshaped = np.reshape(xs, (-1, xs.shape[0], xs.shape[1]))
     n_pitches = int((xs.shape[1] - 2) / 2)
 
-    xs_reshaped = np.reshape(xs, (-1, xs.shape[0], xs.shape[1]))
+    # predict
+    pred = model.predict(xs_reshaped, verbose=0)
+    pred = np.insert(pred, 1, 0)
+    pred = pred.reshape(1, -1)
 
-    pred = model.predict(xs_reshaped)
-    pred = np.insert(pred, 1, (xs[-1, 1] + pred[0, 0] % 1))
-    pred[2:2 + n_pitches] = np.round(pred[2:2 + n_pitches])
+    # rescale prediction
+    pred = scaler.inverse_transform(pred)
+
+    # calculate time offset
+    pred[0, 1] = (xs[-1, 1] + pred[0, 0]) % 1
+
+    # round note states
+    pred[0, 2:2 + n_pitches] = np.round(pred[0, 2:2 + n_pitches])
 
     return pred
+
+
+def generate_notes(model: Model, scaler: ColumnScaler, sample_notes: np.ndarray, n_generate: int = 10):
+    result = sample_notes.copy()
+
+    for _ in tqdm(range(n_generate)):
+        sample = result[-sample_notes.shape[0]:]
+        next_note = predict_note(model, scaler, sample)
+        result = np.vstack((result, next_note))
+
+    return result[sample_notes.shape[0]:]
