58b3048f01c208e1cbae31867561c06ba6e9a393
Adrian Ćwiąkała <61947288+adcw@users.noreply.github.com>
2023-06-07 22:42:33 +0200

    Update model

--- src/model.py
+++ src/model.py
@@ -12,28 +12,30 @@ def get_model(xs: np.ndarray, ys: np.ndarray, validation_data: tuple[np.ndarray,
     if model_name is None:
         model_name = "h5_models/music_lstm.h5"
     if not load:
+
         input_shape = xs[0].shape
         output_shape = ys[0].shape
 
         cnt = int((input_shape[1] - 2) / 2)
 
         inputs = Input(shape=input_shape)
-        lstm_out1 = LSTM(64, dropout=0.2, return_sequences=True)(inputs)
-        lstm_out2 = LSTM(32, dropout=0.2)(lstm_out1)
-        outputs = Dense(output_shape[1])(lstm_out2)
+        lstm_out1 = LSTM(64, dropout=0, return_sequences=True)(inputs)
+        lstm_out2 = LSTM(64, dropout=0.2)(lstm_out1)
+        outputs = Dense(output_shape[1], activation='sigmoid')(lstm_out2)
 
-        selected_outputs = outputs[:, 2: 2 + cnt]
-        selected_outputs = Activation('softmax')(selected_outputs)
-        outputs = tf.concat([outputs[:, :2], selected_outputs, outputs[:, 2 + cnt:]], axis=1)
+        # selected_outputs = outputs[:, 2: 2 + cnt]
+        # selected_outputs = Activation('softmax')(selected_outputs)
+        # outputs = tf.concat([outputs[:, :2], selected_outputs, outputs[:, 2 + cnt:]], axis=1)
 
-        early_stopping = EarlyStopping(monitor="val_loss", patience=10)
+        early_stopping = EarlyStopping(monitor="val_loss", patience=6)
 
         model = Model(name=model_name, inputs=inputs, outputs=outputs)
-        model.compile(optimizer=Adam(learning_rate=0.008), loss="mse")
+        model.compile(optimizer=Adam(learning_rate=0.01), loss="mse")
         model.summary()
         model.save(model_name)
 
-        history = model.fit(x=xs, y=ys, epochs=100, callbacks=[early_stopping], validation_data=validation_data)
+        history = model.fit(x=xs, y=ys, epochs=500, callbacks=[early_stopping], validation_data=validation_data,
+                            batch_size=1)
 
         loss = history.history["loss"]
         epochs = range(len(loss))
