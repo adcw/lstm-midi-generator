59da923090622383fb79c5611cd03640689aedbd
Adrian Ćwiąkała <61947288+adcw@users.noreply.github.com>
2023-06-07 22:44:38 +0200

    Update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from src.dataset import read_datasets, notes_to_dataset, dataset_to_notes
 from src.midi_func import notes_to_midi
 from src.model import get_model
 from src.utils import get_midi_filenames
-from src.generate import generate
+from src.generate import predict_note, generate_notes
 
 VOCAB_SIZE = 128
 SEQ_LEN = 40
@@ -13,16 +13,16 @@ RESOLUTION = 480
 TEMPO = 172
 
 if __name__ == '__main__':
-    filenames = get_midi_filenames(main_dir='samples', subdirs=['Basic Beats'])
+    filenames = get_midi_filenames(main_dir='samples', subdirs=['Other'])
 
     all_notes = read_datasets(filepaths=filenames)
-    dataset = notes_to_dataset(all_notes[8])
+    dataset = notes_to_dataset(all_notes[2])
 
     # notes = dataset_to_notes(dataset)
     # notes_to_midi(notes, instrument_name="Acoustic Grand Piano", resolution=RESOLUTION, tempo=TEMPO,
     #               out_file="./output/test.mid")
 
-    input_len = 70
+    input_len = 40
     xs, ys, scaler = training_sequence(dataset, input_len=input_len, output_len=1)
 
     [n_rows_input, n_cols_input] = xs[0].shape
@@ -33,7 +33,6 @@ if __name__ == '__main__':
 
     model = get_model(xs=x_train, ys=y_train, validation_data=(x_val, y_val), load=False)
 
-    # generate note
-    generate(model=model, scaler=scaler, sample_notes=all_notes[8], input_len=n_rows_input, output_len=n_rows_output)
+    notes = generate_notes(model=model, scaler=scaler, sample_notes=x_test[0], n_generate=30)
 
     pass
