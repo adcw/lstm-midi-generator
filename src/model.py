00b8c2f1f0bc5b7d550a9e65c7207055baa4f9e9
Adrian Ćwiąkała <61947288+adcw@users.noreply.github.com>
2023-06-08 13:44:20 +0200

    get_model: save trained model

--- src/model.py
+++ src/model.py
@@ -4,14 +4,15 @@ from keras.callbacks import EarlyStopping
 from keras.layers import Input, LSTM, Dense, Activation, Conv1D
 from keras.models import Model, load_model
 from keras.optimizers import Adam
+import pickle
 import tensorflow as tf
 
 
-def get_model(xs: np.ndarray, ys: np.ndarray, validation_data: tuple[np.ndarray, np.ndarray] | None = None, load=False,
+def get_model(xs: np.ndarray, ys: np.ndarray, validation_data: tuple[np.ndarray, np.ndarray] | None = None, init=True,
               model_name: str | None = None):
     if model_name is None:
         model_name = "h5_models/music_lstm.h5"
-    if not load:
+    if init:
 
         input_shape = xs[0].shape
         output_shape = ys[0].shape
@@ -32,10 +33,11 @@ def get_model(xs: np.ndarray, ys: np.ndarray, validation_data: tuple[np.ndarray,
         model = Model(name=model_name, inputs=inputs, outputs=outputs)
         model.compile(optimizer=Adam(learning_rate=0.01), loss="mse")
         model.summary()
-        model.save(model_name)
 
         history = model.fit(x=xs, y=ys, epochs=500, callbacks=[early_stopping], validation_data=validation_data,
                             batch_size=1)
+
+        model.save(model_name)
 
         loss = history.history["loss"]
         epochs = range(len(loss))
