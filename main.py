9771297ad9d9f3748fc3d75efe5828208a697b48
Przemek <rysiulaw@gmail.com>
2023-06-08 09:39:34 +0200

    prototype saving

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import keras
 from sklearn.model_selection import train_test_split
 
 from src.data_sequences import training_sequence
@@ -6,6 +7,7 @@ from src.midi_func import notes_to_midi
 from src.model import get_model
 from src.utils import get_midi_filenames
 from src.generate import predict_note, generate_notes
+import pickle
 
 VOCAB_SIZE = 128
 SEQ_LEN = 40
@@ -13,7 +15,7 @@ RESOLUTION = 480
 TEMPO = 172
 
 if __name__ == '__main__':
-    filenames = get_midi_filenames(main_dir='samples', subdirs=['Other'])
+    filenames = get_midi_filenames(main_dir='samples', subdirs=['Basic Beats'])
 
     all_notes = read_datasets(filepaths=filenames)
     dataset = notes_to_dataset(all_notes[2])
@@ -32,7 +34,13 @@ if __name__ == '__main__':
     x_val, x_test, y_val, y_test = train_test_split(x_test, y_test, test_size=0.5)
 
     model = get_model(xs=x_train, ys=y_train, validation_data=(x_val, y_val), load=False)
+    model.save("mymodel.h5")
+    with open('scaler.pkl', 'wb') as f:
+        pickle.dump(scaler, f)
 
+    with open('scaler.pkl', 'rb') as f:
+        scaler2 = pickle.load(f)
+    a = keras.models.load_model("mymodel.h5")
     notes = generate_notes(model=model, scaler=scaler, sample_notes=x_test[0], n_generate=30)
-
+    notes2 = generate_notes(model=a, scaler=scaler2, sample_notes=x_test[0], n_generate=30)
     pass
