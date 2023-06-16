ce8e31e38c5b44141b489685f68d9bd2d96cf5f2
Adrian Ćwiąkała <61947288+adcw@users.noreply.github.com>
2023-06-16 13:45:35 +0200

    Remove unused ColumnScaler

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,7 @@
-import pathlib
 import glob
+import pathlib
+
 import numpy as np
-import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
 
 
 def get_midi_filenames(main_dir: str, subdirs: list[str] | None = None):
@@ -11,51 +10,3 @@ def get_midi_filenames(main_dir: str, subdirs: list[str] | None = None):
         return glob.glob(str(data_dir / '*/*.mid*'))
 
     return np.array([glob.glob(str(main_dir + '/' + d + '/*.mid')) for d in subdirs]).flatten()
-
-
-class ColumnScaler(MinMaxScaler):
-    """
-    A scaler with the option to specify column names to be scaled.
-    """
-    def __init__(self):
-        super().__init__()
-        self.cols: list[str] | None = None
-        self.indexes: list[int] | None = None
-
-    def fit(self, df: pd.DataFrame, y=None, cols: list[str] | None = None):
-        self.cols = cols
-        self.indexes = [df.columns.get_loc(col) for col in self.cols] if cols is not None else None
-
-        df = self._pick_cols(df) if cols is not None else df
-        return super().fit(df, y)
-
-    def transform(self, df: pd.DataFrame | np.ndarray):
-        if self.cols is not None:
-            to_transform = self._pick_cols(df)
-            transformed = super().transform(to_transform)
-
-            if isinstance(df, pd.DataFrame):
-                df.iloc[:, self.indexes] = transformed
-            if isinstance(df, np.ndarray):
-                df = np.insert(df, self.indexes, transformed)
-        else:
-            df = super().transform(df)
-        return df
-
-    def inverse_transform(self, row: np.ndarray):
-        if self.cols is not None:
-            row_to_transform = self._pick_cols(row)
-            row_to_transform = row_to_transform.reshape(1, -1)
-            transformed = super().inverse_transform(row_to_transform)
-
-            row[self.indexes] = transformed
-        else:
-            row = super().inverse_transform(row)
-        return row
-
-    def _pick_cols(self, data: pd.DataFrame | np.ndarray | None = None):
-        if isinstance(data, pd.DataFrame):
-            data = data[self.cols]
-        if isinstance(data, np.ndarray):
-            data = data[self.indexes]
-        return data
